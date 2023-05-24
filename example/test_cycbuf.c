61fe68d9f164828eeba5ddcb6445d768f2227209
enguo.shan <enguo.shan@freetech.com>
2023-05-25 00:10:51 +0800

    更新cycbuf测试程序

--- example/test_cycbuf.c
+++ example/test_cycbuf.c
@@ -1,6 +1,6 @@
 #include "utils_cycbuf.h"
 
-char *buf[8] = {"11111", "22222", "33333", "44444", "55555", "66666" "77777", "88888"};
+char *buf[10] = {"11111", "22222", "33333", "44444", "55555", "66666", "77777", "88888", "99999", "aaaaa"};
 
 void test_cycbuf(void)
 {
@@ -10,30 +10,57 @@ void test_cycbuf(void)
 
     printf("++++++++++++++++++++++++util_cycbuf_push++++++++++++++++++++++++\n");
     for (int i = 0; i < 10; i++)
-    {
-        printf("util_cycbuf_push=%d %d\n", util_cycbuf_push(&cycbuf, buf[i], 5), util_cycbuf_size(&cycbuf));
-    }
-
-    printf("++++++++++++++++++++++++util_cycbuf_pop++++++++++++++++++++++++\n");
-    for (int i = 0; i < 10; i++)
-    {
-        char tmp[128] = {0};
-        util_cycbuf_pop(&cycbuf, tmp, 10);
-        printf("util_cycbuf_pop=%d %d tmp=%s\n", util_cycbuf_pop(&cycbuf, tmp, 5), util_cycbuf_size(&cycbuf), tmp);
+    {   
+        uint32_t len = util_cycbuf_push(&cycbuf, buf[i], 5);
+        printf("util_cycbuf_push=%d %d\n", len, util_cycbuf_size(&cycbuf));
+        util_cycbuf_print(&cycbuf);
     }
 
     printf("++++++++++++++++++++++++util_cycbuf_peek++++++++++++++++++++++++\n");
     for (int i = 0; i < 10; i++)
     {
         char tmp[128] = {0};
-        printf("util_cycbuf_peek=%d %d tmp=%s \n", util_cycbuf_peek(&cycbuf, tmp, 5), util_cycbuf_size(&cycbuf), tmp);
+        uint32_t len = util_cycbuf_peek(&cycbuf, tmp, 5);
+        printf("util_cycbuf_peek=%d %d tmp=%s \n", len, util_cycbuf_size(&cycbuf), tmp);
+        util_cycbuf_print(&cycbuf);
+    }
+
+    printf("++++++++++++++++++++++++util_cycbuf_pop++++++++++++++++++++++++\n");
+    for (int i = 0; i < 10; i++)
+    {
+        char tmp[128] = {0};
+        uint32_t len = util_cycbuf_pop(&cycbuf, tmp, 5);
+        printf("util_cycbuf_pop=%d %d tmp=%s\n", len, util_cycbuf_size(&cycbuf), tmp);
+        util_cycbuf_print(&cycbuf);
+    }
+
+    printf("++++++++++++++++++++++++util_cycbuf_pop++++++++++++++++++++++++\n");
+    for (int i = 0; i < 10; i++)
+    {   
+        uint32_t len = util_cycbuf_push(&cycbuf, buf[i], 5);
+        printf("util_cycbuf_push=%d %d\n", len, util_cycbuf_size(&cycbuf));
+        util_cycbuf_print(&cycbuf);
+
+        if (0 == i%2) {
+        char tmp[128] = {0};
+        uint32_t len = util_cycbuf_pop(&cycbuf, tmp, 5);
+        printf("util_cycbuf_pop=%d %d tmp=%s\n", len, util_cycbuf_size(&cycbuf), tmp);
+            util_cycbuf_print(&cycbuf);
+        }
     }
 
     printf("++++++++++++++++++++++++util_cycbuf_lose++++++++++++++++++++++++\n");
     for (int i = 0; i < 10; i++)
-    {
-        util_cycbuf_lose(&cycbuf, 10);
-        printf("util_cycbuf_lose=%d %d \n", util_cycbuf_lose(&cycbuf, 10), util_cycbuf_size(&cycbuf));
+    {   
+        uint32_t len = util_cycbuf_push(&cycbuf, buf[i], 5);
+        printf("util_cycbuf_push=%d %d\n", len, util_cycbuf_size(&cycbuf));
+        util_cycbuf_print(&cycbuf);
+
+        if (0 == i%2) {
+            uint32_t len = util_cycbuf_lose(&cycbuf, 5);
+            printf("util_cycbuf_lose=%d %d \n", len, util_cycbuf_size(&cycbuf));
+            util_cycbuf_print(&cycbuf);
+        }
     }
 
     util_cycbuf_deinit(&cycbuf);
