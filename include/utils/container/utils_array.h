1f1e06258762ff1fce525745c3e2028a0a5cfac5
solo <416780590@qq.com>
2023-10-25 23:57:34 +0800

    新增utils_array接口

--- include/utils/container/utils_array.h
+++ include/utils/container/utils_array.h
@@ -1,17 +1,143 @@
+/**
+ * @file utils_array.h
+ * @author enguo.shan (enguo.shan@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-25
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #ifndef H_UTILS_ARRAY_H
 #define H_UTILS_ARRAY_H
 
-#include <stddef.h>
+#include <assert.h>
+#include <string.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct util_array_s;
-typedef struct util_array_s util_array_t;
+typedef int (*onUtilArrayComparCallback) (const void *, const void *);
 
-int util_array_create(util_array_t **array, bool zero_terminated, bool clear, size_t elt_size, size_t reserved_size);
+#define utils_array_dec(name, T)                                       										\
+	struct utils_array_##name {                                        										\
+		bool oom;                                                      										\
+		size_t cap;                                                    										\
+		size_t size;                                                   										\
+		/* NOLINTNEXTLINE */                                           										\
+		T *elems;                                                      										\
+	};																   										\
+											   																\
+	/**																										\
+	 * Init array																							\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_create_##name(struct utils_array_##name *array);										\
+																											\
+	/**																										\
+	 * Destroy array																						\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_destroy_##name(struct utils_array_##name *array);										\
+																											\
+	/**																										\
+	 * Add elem to array, call utils_array_oom(v) to see if 'add' failed because of out						\
+	 * of memory.																							\
+	 *																										\
+	 * @param a array																						\
+	 * @param k elem																						\
+	 */																										\
+	int utils_array_add_##name(struct utils_array_##name *array, T elem);                     				\
+																											\
+	/**																										\
+	 * Deletes items from the array without deallocating underlying memory									\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_clear_##name(struct utils_array_##name *array);                          				\
+																											\
+	/**																										\
+	 *   @param a array																						\
+	 *   @param i element index, If 'i' is out of the range, result is undefined.							\
+	 */																										\
+	int utils_array_del_##name(struct utils_array_##name *array, uint32_t i);                				\
+																											\
+	/**																										\
+	 * Deletes the element at index i, replaces last element with the deleted								\
+	 * element unless deleted element is the last element. This is faster than								\
+	 * moving elements but elements will no longer be in the 'add order'									\
+	 *																										\
+	 * arr[a,b,c,d,e,f] -> utils_array_del_unordered(arr, 2) - > arr[a,b,f,d,e]								\
+	 *																										\
+	 * @param a array																						\
+	 * @param i index. If 'i' is out of the range, result is undefined.										\
+	 */																										\
+	int utils_array_del_unordered_##name(struct utils_array_##name *array, uint32_t i);            			\
+																											\
+	/**																										\
+	 * Deletes the last element. If current size is zero, result is undefined.								\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_del_last_##name(struct utils_array_##name *array);                             			\
+																											\
+	/**																										\
+	 * Sorts the array using qsort()																		\
+	 * @param a   array																						\
+	 * @param cmp comparator, check qsort() documentation for details										\
+	 */																										\
+	int utils_array_sort_##name(struct utils_array_##name *array, onUtilArrayComparCallback cmp);
+
+/**
+ * @param a array
+ * @return true if last add operation failed, false otherwise.
+ */
+#define utils_array_oom(array) ((array)->oom)
+
+/**
+ * Get element at index i, if 'i' is out of range, result is undefined.
+ *
+ * @param a array
+ * @param i index
+ * @return element at index 'i'
+ */
+#define sc_array_at(array, i) ((array)->elems[i])
+
+/**
+ * @param a array
+ * @return element count
+ */
+#define sc_array_size(array) ((array)->size)
+
+/**
+ * Returns last element. If array is empty, result is undefined.
+ * @param a array
+ */
+#define sc_array_last(array) (array)->elems[(array)->size - 1]
+
+/**
+ * @param a    array
+ * @param elem elem
+ */
+#define utils_array_foreach(array, elem)                                    \
+	for (size_t _k = 1, _i = 0; _k && _i != (array)->size; _k = !_k, _i++)  \
+		for ((elem) = (array)->elems[_i]; _k; _k = !_k)
+
+//        (name, value type)
+utils_array_dec(int, 	int)
+utils_array_dec(uint, 	unsigned int)
+utils_array_dec(long, 	long)
+utils_array_dec(ll, 	long long)
+utils_array_dec(ulong, 	unsigned long)
+utils_array_dec(ull, 	unsigned long long)
+utils_array_dec(32, 	uint32_t)
+utils_array_dec(64, 	uint64_t)
+utils_array_dec(double, double)
+utils_array_dec(str, 	const char *)
+utils_array_dec(ptr, 	void *)
 
 #ifdef __cplusplus
 }
--- src/container/utils_array.c
+++ src/container/utils_array.c
@@ -1,5 +1,178 @@
 #include "utils_array.h"
 
-struct util_array_s {
+#define UTIL_ARRAY_MAX SIZE_MAX
 
-};
+#define utils_array_def(name, T)                                       										\
+											   																\
+	/**																										\
+	 * Init array																							\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_create_##name(struct utils_array_##name *array) 										\
+    {                                                                                                       \
+        if (NULL == array) {                                                                                \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+        memset((array), 0, sizeof(*(array)));                                                               \
+        return 0;                                                                                           \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 * Destroy array																						\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_destroy_##name(struct utils_array_##name *array)										\
+    {                                                                                                       \
+        if (NULL == array) {                                                                                \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+        if (NULL != (array)->elems) {                                                                       \
+            free((array)->elems);                                                                           \
+        }                                                                                                   \
+		                                                                                                    \
+		memset((array), 0, sizeof(*(array)));                                                               \
+        return 0;                                                                                           \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 * Add elem to array, call utils_array_oom(v) to see if 'add' failed because of out						\
+	 * of memory.																							\
+	 *																										\
+	 * @param a array																						\
+	 * @param k elem																						\
+	 */																										\
+	int utils_array_add_##name(struct utils_array_##name *array, T elem)                      				\
+    {                                                                                                       \
+		size_t _max = 0;                                                                                    \
+		size_t _cap;                                                                                        \
+		void *_p;                                                                                           \
+                                                                                                            \
+        if (NULL == array) {                                                                                \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+        _max = UTIL_ARRAY_MAX / sizeof(*(array)->elems);                                                      \
+		if ((array)->cap == (array)->size) {                                                                \
+			if ((array)->cap > _max / 2) {                                                                  \
+				(array)->oom = true;                                                                        \
+				return -1;                                                                                  \
+			}                                                                                               \
+			_cap = (array)->cap == 0 ? 8 : (array)->cap * 2;                                                \
+			_p = realloc((array)->elems, _cap * sizeof(*((array)->elems)));                                 \
+			if (_p == NULL) {                                                                               \
+				(array)->oom = true;                                                                        \
+				return -1;                                                                                  \
+			}                                                                                               \
+			(array)->cap = _cap;                                                                            \
+			(array)->elems = _p;                                                                            \
+		}                                                                                                   \
+		(array)->oom = false;                                                                               \
+		(array)->elems[(array)->size++] = elem;                                                             \
+                                                                                                            \
+        return 0;                                                                                           \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 * Deletes items from the array without deallocating underlying memory									\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_clear_##name(struct utils_array_##name *array)                          				\
+    {                                                                                                       \
+        if (NULL == array) {                                                                                \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+		(array)->size = 0;                                                                                  \
+		(array)->oom = false;                                                                               \
+                                                                                                            \
+        return 0;                                                                                           \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 *   @param a array																						\
+	 *   @param i element index, If 'i' is out of the range, result is undefined.							\
+	 */																										\
+	int utils_array_del_##name(struct utils_array_##name *array, uint32_t i)                				\
+    {                                                                                                       \
+        if (NULL == array || i >= (array)->size) {                                                          \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+        size_t idx = (i);                                                                                   \
+                                                                                                            \
+		const size_t _cnt = (array)->size - (idx) - 1;                                                      \
+		if (_cnt > 0) {                                                                                     \
+			memmove(&((array)->elems[idx]), &((array)->elems[idx + 1]),                                     \
+				_cnt * sizeof(*((array)->elems)));                                                          \
+		}                                                                                                   \
+		(array)->size--;                                                                                    \
+                                                                                                            \
+        return (array)->size;                                                                               \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 * Deletes the element at index i, replaces last element with the deleted								\
+	 * element unless deleted element is the last element. This is faster than								\
+	 * moving elements but elements will no longer be in the 'add order'									\
+	 *																										\
+	 * arr[a,b,c,d,e,f] -> utils_array_del_unordered(arr, 2) - > arr[a,b,f,d,e]								\
+	 *																										\
+	 * @param a array																						\
+	 * @param i index. If 'i' is out of the range, result is undefined.										\
+	 */																										\
+	int utils_array_del_unordered_##name(struct utils_array_##name *array, uint32_t i)            			\
+    {                                                                                                       \
+        if (NULL == array || i >= (array)->size) {                                                          \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+        size_t idx = (i);                                                                                   \
+		(array)->elems[idx] = (array)->elems[(--(array)->size)];                                            \
+                                                                                                            \
+        return 0;                                                                                           \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 * Deletes the last element. If current size is zero, result is undefined.								\
+	 * @param a array																						\
+	 */																										\
+	int utils_array_del_last_##name(struct utils_array_##name *array)                             			\
+    {                                                                                                       \
+        if (NULL == array || 0 == (array)->size) {                                                          \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+		(array)->size--;                                                                                    \
+        return 0;                                                                                           \
+    }                                                                                                       \
+																											\
+	/**																										\
+	 * Sorts the array using qsort()																		\
+	 * @param a   array																						\
+	 * @param cmp comparator, check qsort() documentation for details										\
+	 */																										\
+	int utils_array_sort_##name(struct utils_array_##name *array, onUtilArrayComparCallback cmp)      	    \
+    {                                                                                                       \
+        if (NULL == array || 0 == (array)->size || NULL == cmp) {                                           \
+            return -1;                                                                                      \
+        }                                                                                                   \
+                                                                                                            \
+        qsort((array)->elems, (array)->size, sizeof(*(array)->elems), cmp);                                 \
+        return 0;                                                                                           \
+    }
+
+
+//        (name, value type)
+utils_array_def(int, 	int)
+utils_array_def(uint, 	unsigned int)
+utils_array_def(long, 	long)
+utils_array_def(ll, 	long long)
+utils_array_def(ulong, 	unsigned long)
+utils_array_def(ull, 	unsigned long long)
+utils_array_def(32, 	uint32_t)
+utils_array_def(64, 	uint64_t)
+utils_array_def(double, double)
+utils_array_def(str, 	const char *)
+utils_array_def(ptr, 	void *)
